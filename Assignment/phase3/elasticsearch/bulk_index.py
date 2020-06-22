c478d0c9133beb8099bb3cad4fbe5641e17b8df4
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-06-23 01:07:35 +0430

    [phase3] section2 is completed

---
+++ Assignment/phase3/elasticsearch/bulk_index.py
@@ -0,0 +1,22 @@
+from elasticsearch import Elasticsearch, helpers
+import json
+
+es = Elasticsearch("localhost:9200")
+
+def delete_index():
+    es.indices.delete(index='paper')
+
+
+def index_data():
+    indexed_docs = list()
+    with open('./data.json', 'r') as json_file:
+        docs = json.load(json_file)
+    
+    for index, doc in enumerate(docs):
+        doc_str = json.dumps(doc)
+        print(doc_str)
+        es.index(index='paper', id=index, body=doc)
+
+
+index_data()
+
