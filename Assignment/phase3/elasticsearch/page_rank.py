cb9006614c2e153ba4f650d982907c7eb5227d6b
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-06-24 19:12:52 +0430

    section 3 completed

--- Assignment/phase3/elasticsearch/page_rank.py
+++ Assignment/phase3/elasticsearch/page_rank.py
@@ -5,10 +5,10 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch('localhost:9200')
 
 # Reading data from file
-with open('./data.json', 'r') as json_file:
+with open('./elasticsearch/data.json', 'r') as json_file:
     data = json.load(json_file)
 
-# Creating a list of IDs
+
 N = len(data)
 a = np.zeros(shape=(N, N))
 v = np.ones(shape=(1, N))
@@ -17,17 +17,18 @@ x[0][0] = 1
 alpha = 0.1
 papers = set()
 
+# Creating a list of IDs
 for paper_info in data:
     paper_id = paper_info['paper']['id']
     papers.add(paper_id)
 
 # Map each ID to index
 paper_id_map = dict()
-index = 0
-for paper in papers:
+for index, paper in enumerate(papers):
     paper_id_map[paper] = index
     index += 1
 
+    
 # calculate adjacency matrix
 def len_calc(refs):
     valid_len = len(refs)
@@ -37,28 +38,36 @@ def len_calc(refs):
             valid_len -= 1
     return valid_len
 
+dead_ends = list()
+
 for paper_info in data:
     paper_id = paper_info['paper']['id']
+    paper_index = paper_id_map[paper_id]
     paper_refs = paper_info['paper']['references']
     refs_no = len_calc(paper_refs)
     
+    is_deadend = True
     for paper_ref in paper_refs:
         reference = paper_ref.split('/')[-1]
         reference_no = len(paper)
         if reference in papers:
+            is_deadend = False
             ref_index = paper_id_map[reference]
-            paper_index = paper_id_map[paper_id]
-            a[paper_index][ref_index] = 1 / refs_no
-
+            a[paper_index][ref_index] = 1/refs_no
+            a[paper_index][ref_index] = (1 - alpha) * a[paper_index][ref_index] + (alpha * 1 / N)
+    
+    if is_deadend:
+        a[paper_index] = np.array([1/N] * N)
 p = a[:]
 
 # calculate pi = (1  - alpha) * pi + alpha * v/N
 for i, row in enumerate(p):
-    p[i][:] = (1 - alpha) * row + (alpha * (v / N))
+    if i not in dead_ends:
+        p[i][:] = (1 - alpha) * row + (alpha * (v / N))
     
 #matrix
 page_ranks = np.dot(x, p)
-for i in range(10):
+for _ in range(10):
     page_ranks = np.dot(page_ranks, p)
 
 for i in range(N):
