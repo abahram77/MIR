2bca45fb4b433ae821c6d207112c77dcdda815cf
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-07-21 13:09:35 +0430

    [phase3] missed "match" and "range" keywords added

--- Assignment/MIR_project3/elasticsearch/query.py
+++ Assignment/MIR_project3/elasticsearch/query.py
@@ -3,46 +3,46 @@ from elasticsearch import Elasticsearch
 
 def search(es_url, abstract_weight, abstract_search, title_weight, title_search, date_weight, date_search):
     es = Elasticsearch(es_url)
-    tie_breaker = input('Enter a number between 0 to 1 for tiebreaker, or leave it empty for the default value')
-    if not tie_breaker:
-        tie_breaker = 0.5
-    else:
-        tie_breaker = float(tie_breaker)
+    tie_breaker = 0.5
     search_params = {
-        'query': {
-            'size': 10,
-            'from': 0,
-            'nested': {
-                'path': 'paper',
-                'query': {
-                    'dis_max': {
-                        'queries': [
+            "query": {
+            "nested": {
+                "path": "paper",
+                "query": {
+                    "dis_max": {
+                        "queries": 
+                        [
                             {
-                                'paper.abstract': {
-                                    'query': abstract_search,
-                                    'boost': abstract_weight
+                                "match": {
+                                    "paper.title": {
+                                        "query": title_search, 
+                                        "boost": title_weight
+                                    }
                                 }
                             },
                             {
-                                'paper.title': {
-                                    'query': title_search,
-                                    'boost': title_weight
+                                "match": {
+                                    "paper.abstract": {
+                                        "query": abstract_search, 
+                                        "boost": abstract_weight
+                                    }
                                 }
                             },
                             {
-                                'paper.date': {
-                                    'query': date_search,
-                                    'boost': date_weight
+                                "range": {
+                                    "paper.date": {
+                                        "gte": date_search, 
+                                        "boost": date_weight
+                                    }
                                 }
-                            },
+                            }
                         ],
-                        'tie-breaker': tie_breaker
+                        "tie_breaker": 0.5
                     }
                 }
             }
         }
     }
-
 
     result = es.search(index='paper', body=search_params)
     return result
@@ -52,13 +52,17 @@ def search(es_url, abstract_weight, abstract_search, title_weight, title_search,
 # Variables 
 ES_URL = 'localhost:9200'
 
-abstract_weight = input("please enter the abstract weight:\n")
+abstract_weight = float(input("please enter the abstract weight:\n"))
 abstract_search = input("please enter the keywords from abstract:\n")
 
-title_weight = input("please enter the title weight:\n")
+title_weight = float(input("please enter the title weight:\n"))
 title_search = input("please enter the keywords from title:\n")
 
-date_weight = input("please enter the date weight:\n")
+date_weight = float(input("please enter the date weight:\n"))
 date_search = input("please enter the paper date:\n")
 
-search(ES_URL, abstract_weight, abstract_search, title_weight, title_search, date_weight, date_search)
+result = search(ES_URL, abstract_weight, abstract_search, title_weight, title_search, date_weight, date_search)
+
+for doc in result['hits']['hits']:
+    paper = doc['_source']['paper']
+    print('title:',paper['title'], 'date:', paper['date'])
